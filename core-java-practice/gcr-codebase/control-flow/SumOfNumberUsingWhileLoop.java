8edf6b63e7c089ffd293bd8050aaa04973020f56
Amit Kumar Rai <raiamitrai1001@gmail.com>
2025-12-19 19:29:40 +0530

    [Amit Kumar Rai] Add .Added java code for sum of number using while loop

---
+++ core-java-practice/gcr-codebase/control-flow/SumOfNumberUsingWhileLoop.java
@@ -0,0 +1,25 @@
+//Write a program to find the sum of numbers until the user enters 0
+//Hint =>
+//Create a variable total of type double initialize to 0.0. Also, create a variable to store the double value the user enters
+//Use the while loop to check if the user entered is 0
+//If the user entered value is not 0 then inside the while block add user entered value to the total and ask the user to input again
+//The loop will continue till the user enters zero and outside the loop display the total value
+
+package controlflow;
+
+import java.util.Scanner;
+
+class SumOfNumberUsingWhileLoop {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double total = 0.0;
+        while (true) {
+            double num = input.nextDouble();
+            if (num == 0.0) {
+                break;
+            }
+            total += num;
+        }
+        System.out.println("The total sum is " + total);
+    }
+}
