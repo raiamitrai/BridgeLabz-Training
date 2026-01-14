6260f5ca0f8a708029ad2a99dcdcd92e76f3b607
Amit Kumar Rai <raiamitrai1001@gmail.com>
2026-01-14 12:45:30 +0530

    [Amit Kumar Rai] add. added the Extract date, capatilized word, email from the text

---
+++ collection-practice/gcr-codebase/regex-and-junit/DateExtract.java
@@ -0,0 +1,18 @@
+package regex;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class DateExtract {
+    public static void main(String[] args) {
+
+        String text = "The events are scheduled for 12/05/2023, 15/08/2024, and 29/02/2020.";
+        String regex = "\\b\\d{2}/\\d{2}/\\d{4}\\b";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(text);
+
+        while(matcher.find()){
+            System.out.println(matcher.group());
+        }
+    }
+}
