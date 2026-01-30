39db072b15690fb2f1432080befa937bf4916f81
Amit Kumar Rai <raiamitrai1001@gmail.com>
2026-01-30 12:38:08 +0530

    [Amit Kumar Rai] Add. added factory robot hazard analyzer problem

---
+++ dsa-practice/scenario-based/factory_Robot_hazard_analyzer/RobotSafetyException.java
@@ -0,0 +1,7 @@
+package dsa.scenario.factory_Robot_hazard_analyzer;
+public class RobotSafetyException extends Exception{
+    public RobotSafetyException(String message){
+        super(message);
+    }
+
+}
