59f92cd61637e3a306724b7aab5faecba4ae7bc6
Amit Kumar Rai <raiamitrai1001@gmail.com>
2026-07-31 15:50:46 +0530

    [Amit Kumar Rai] added hackerrank solution of java String Tockens

---
+++ Day1/JavaStringTockens/Solution.java
@@ -0,0 +1,30 @@
+package Day1.JavaStringTockens;
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine();
+        // Write your code here.
+        s = s.trim();
+
+        if (s.length() == 0) {
+            System.out.println(0);
+            return;
+        }
+
+        String[] tokens = s.split("[ !,?._'@]+");
+
+        System.out.println(tokens.length);
+
+        for (String str : tokens) {
+            System.out.println(str);
+        }
+
+    }
+}
+
+
