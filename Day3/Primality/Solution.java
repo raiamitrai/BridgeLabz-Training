d6e62f7370248ca0513603ecb12c194f1686d3fa
Amit Kumar Rai <raiamitrai1001@gmail.com>
2026-08-04 21:25:45 +0530

    [Amit Kumar Rai] Added. hackerrank solution (Primality)

---
+++ Day3/Primality/Solution.java
@@ -0,0 +1,66 @@
+package Day3.Primality;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'primality' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts INTEGER n as parameter.
+     */
+
+    public static String primality(int n) {
+    // Write your code here
+    if (n < 2) {
+        return "Not prime";
+    }
+
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            return "Not prime";
+        }
+    }
+
+    return "Prime";
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int p = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, p).forEach(pItr -> {
+            try {
+                int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+                String result = Result.primality(n);
+
+                bufferedWriter.write(result);
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+
