7dccb9b2e8495f2144cf87988c3cae340c17fb98
Amit Kumar Rai <raiamitrai1001@gmail.com>
2026-08-10 10:45:53 +0530

    [Amit Kumar Rai] Added. hackerrank solution ( recursion : fibonacci number )

---
+++ Day7/Recursion_Fibonacci_Numbers/Solution.java
@@ -0,0 +1,22 @@
+package Day7.Recursion_Fibonacci_Numbers;
+
+import java.util.*;
+
+public class Solution {
+
+    public static int fibonacci(int n) {
+        
+        if(n <= 1){
+            return n ;
+        }
+        return fibonacci(n -  1 ) + fibonacci( n -2 );
+    }
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        scanner.close();
+        System.out.println(fibonacci(n));
+    }
+}
