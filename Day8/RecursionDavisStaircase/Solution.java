00f50517c538ec950df584a1e23005399a6d98c4
Amit Kumar Rai <raiamitrai1001@gmail.com>
2026-08-11 11:57:14 +0530

    [Amit Kumar Rai] Added. hackerrank solution ( recursion : Davis Staircase )

---
+++ Day8/RecursionDavisStaircase/Solution.java
@@ -0,0 +1,65 @@
+package Day8.RecursionDavisStaircase;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+
+
+
+class Result {
+
+    public static int stepPerms(int n) {
+    // Write your code here
+     if (n == 0) return 1;
+
+    int[] dp = new int[n + 1];
+
+    dp[0] = 1;
+
+    if (n >= 1)
+        dp[1] = 1;
+
+    if (n >= 2)
+        dp[2] = 2;
+
+    for (int i = 3; i <= n; i++) {
+        dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+    }
+
+    return dp[n];
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int s = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, s).forEach(sItr -> {
+            try {
+                int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+                int res = Result.stepPerms(n);
+
+                bufferedWriter.write(String.valueOf(res));
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+
