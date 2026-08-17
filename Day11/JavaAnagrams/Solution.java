bbc4eb7998f6ed7772a710dfabb60341564541bc
Amit Kumar Rai <raiamitrai1001@gmail.com>
2026-08-17 10:55:01 +0530

    [Amit Kumar Rai] Added. hackerrank solution ( Java Anagrams )

---
+++ Day11/JavaAnagrams/Solution.java
@@ -0,0 +1,45 @@
+package Day11.JavaAnagrams;
+
+import java.util.Scanner;
+
+public class Solution {
+
+    static boolean isAnagram(String a, String b) {
+        // Complete the function
+        a = a.toLowerCase();
+        b = b.toLowerCase();
+
+        if (a.length() != b.length()) {
+            return false;
+        }
+
+        java.util.HashMap<Character, Integer> map1 = new java.util.HashMap<>();
+        java.util.HashMap<Character, Integer> map2 = new java.util.HashMap<>();
+
+        for (char ch : a.toCharArray()) {
+            map1.put(ch, map1.getOrDefault(ch, 0) + 1);
+        }
+
+        for (char ch : b.toCharArray()) {
+            map2.put(ch, map2.getOrDefault(ch, 0) + 1);
+        }
+
+        if (map1.equals(map2)) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+  public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        String a = scan.next();
+        String b = scan.next();
+        scan.close();
+        boolean ret = isAnagram(a, b);
+        System.out.println( (ret) ? "Anagrams" : "Not Anagrams" );
+    }
+}
+
