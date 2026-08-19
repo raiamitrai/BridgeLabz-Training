70c11535ca458254b629be829d033c06eaf145fd
Amit Kumar Rai <raiamitrai1001@gmail.com>
2026-08-19 12:05:21 +0530

    [Amit Kumar Rai] Added. hackerrank solution ( Method Overloding )

---
+++ Day13/mathodOverloding/Main.java
@@ -0,0 +1,28 @@
+package Day13.mathodOverloding;
+
+class Calculator {
+
+    int add(int a, int b) {
+        return a + b;
+    }
+
+    double add(double a, double b) {
+        return a + b;
+    }
+
+    int add(int a, int b, int c) {
+        return a + b + c;
+    }
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Calculator c = new Calculator();
+
+        System.out.println(c.add(10, 20));
+        System.out.println(c.add(10.5, 20.5));
+        System.out.println(c.add(10, 20, 30));
+    }
+}
